6630dffe71681d19a22244beaaa0ac8b8ef5535f
R04-YDA-MSI17\nwuser <riru.bra.0401.gm42@gmail.com>
2023-03-18 17:06:45 +0900

    パスミス判定

--- PassGame/Assets/ballManager.cs
+++ PassGame/Assets/ballManager.cs
@@ -1,94 +1,114 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Threading;
-using System.Threading.Tasks;
 
-public class ballManager : MonoBehaviour
-{
-    [SerializeField] float ballSpeed;
-
-    state ballState;
-    
-    // ボールノステータス
-    public enum state
+namespace passGame {
+    public class ballManager : MonoBehaviour
     {
-        dribble,
-        pass,
-    }
+        [SerializeField] float ballSpeed;
 
-    // ボールの初期化
-    public void Setup(state state)
-    {
-        ballState = state;
-    }
+        state ballState;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        // ボールのステータス
+        public enum state
         {
-            if (CheckCanPass())
+            dribble,
+            pass,
+            miss,
+        }
+
+        // ボールの初期化
+        public void Setup(state state)
+        {
+            ballState = state;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
             {
-                BallDeparture(GetVect());
+                if (CheckCanPass())
+                {
+                    BallDeparture(GetVect());
+                }
+            }
+
+            // debug
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ballState = ballState == state.dribble ? state.pass : state.dribble;
+                Debug.Log($"currentState : {ballState}");
             }
         }
 
-        // debug
-        if (Input.GetKeyDown(KeyCode.Space))
+        // ボール発射、ターゲットのポジション方向に移動
+        void BallDeparture(Vector3 taeget)
         {
-            ballState = ballState == state.dribble ? state.pass : state.dribble;
-            Debug.Log($"currentState : {ballState}");
-        }
-    }
+            // 向きの生成（Z成分の除去と正規化）
+            Vector3 shotForward = Vector3.Scale((taeget - transform.position), new Vector3(1, 1, 0)).normalized;
 
-    // ボール発射、ターゲットのポジション方向に移動
-    void BallDeparture(Vector3 taeget)
-    {
-        // 向きの生成（Z成分の除去と正規化）
-        Vector3 shotForward = Vector3.Scale((taeget - transform.position), new Vector3(1, 1, 0)).normalized;
-
-        // 弾に速度を与える
-        GetComponent<Rigidbody2D>().velocity = shotForward * ballSpeed;
-    }
-
-    // マウスのクリック座標取得
-    Vector3 GetVect()
-    {
-        Vector3 mousePosition = Input.mousePosition;
-        // カーソル位置z座標を10に
-        mousePosition.z = 10;
-        // カーソル位置をワールド座標に変換
-        Vector3 target = Camera.main.ScreenToWorldPoint(mousePosition);
-
-        return target;
-    }
-
-    //　ボールのステータス変更
-    public void SetBallState(state state)
-    {
-        this.ballState = state;
-    }
-
-    // パス出し可能か
-    bool CheckCanPass()
-    {
-        bool value = false;
-
-        switch (this.ballState)
-        {
-            case state.dribble:
-                value = true;
-                break;
-
-            case state.pass:
-                value = false;
-                break;
-            default:
-                break;
+            // 弾に速度を与える
+            GetComponent<Rigidbody2D>().velocity = shotForward * ballSpeed;
         }
 
-        Debug.Log($"currntValue : {value}");
-        return value;
+        // マウスのクリック座標取得
+        Vector3 GetVect()
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            // カーソル位置z座標を10に
+            mousePosition.z = 10;
+            // カーソル位置をワールド座標に変換
+            Vector3 target = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            return target;
+        }
+
+        //　ボールのステータス変更
+        public void SetBallState(state state)
+        {
+            this.ballState = state;
+        }
+
+        // パス出し可能か
+        bool CheckCanPass()
+        {
+            bool value = false;
+
+            switch (this.ballState)
+            {
+                case state.dribble:
+                    value = true;
+                    break;
+
+                case state.pass:
+                case state.miss:
+                    value = false;
+                    break;
+                default:
+                    break;
+            }
+
+            Debug.Log($"currntValue : {value}");
+            return value;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+
+            if (collision.name == outareaDefine.outAreaR ||
+                collision.name == outareaDefine.outAreaL)
+            {
+                ballState = state.miss;
+                Debug.Log($"miss");
+            }
+        }
+
+        Vector3 GethitPoint()
+        {
+            Vector3 target = Vector3.zero;
+
+            return target;
+        }
     }
 }
---
+++ PassGame/Assets/outareaDefine.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace passGame
+{
+    public class outareaDefine
+    {
+        public static string outAreaL = "side_L";
+        public static string outAreaR = "side_R";
+    }
+}
