46415e534edebfefa7b4c76d91aa6c0840712d1a
ugatyan <tkugi1479@gmail.com>
2023-03-18 17:08:11 +0900

    Playerたたき

---
+++ PassGame/Assets/Player/Playe_Test.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playe_Test : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float moveLength;
+
+    private Vector2 initialPosition;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector2(initialPosition.x, Mathf.Sin(Time.time * speed) * moveLength + initialPosition.y);
+    }
+}
