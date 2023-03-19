e318d55a37aa47c458f6ad14d17cafa087e54b78
R04-YDA-MSI17\nwuser <riru.bra.0401.gm42@gmail.com>
2023-03-19 10:02:18 +0900

    update branch

--- PassGame/Assets/script/Enemy/EnemyManager.cs
+++ PassGame/Assets/script/Enemy/EnemyManager.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class testScript : MonoBehaviour
+public class EnemyManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //testCode
+        
     }
 
     // Update is called once per frame
